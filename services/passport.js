ca96e2682c8bcce7e798f6ed30f96c31447ca309
Phl3bas <Phl3bas87@gmail.com>
2020-02-13 11:36:53 +0000

    Restructured filestructure

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
-const keys = require('../config/environment');
+const keys = require('./config/environment');
 
 // User model
 const User = mongoose.model('users');
