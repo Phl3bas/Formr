be23e618271ccadb28a29fa6ffb7a978a3332ae1
Phl3bas <Phl3bas87@gmail.com>
2020-02-24 22:04:54 +0000

    complete google auth login flow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './components/Header';
+import { connect } from 'react-redux';
+import * as actions from './actions';
+import LandingPage from './views/LandingPage';
 
-const App = () => {
-  return <div>hi there!</div>;
+const Dashboard = props => {
+  return <h2>Dashboard</h2>;
 };
 
-export default App;
+const SurveyNew = props => {
+  return <h2>SurveyNew</h2>;
+};
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path='/' component={LandingPage} />
+            <Route exact path='/surveys' component={Dashboard} />
+            <Route exact path='/surveys/new' component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
--- routes/auth.google.routes.js
+++ routes/auth.google.routes.js
@@ -8,6 +8,8 @@ router.get(
   })
 );
 
-router.get('/callback', passport.authenticate('google'));
+router.get('/callback', passport.authenticate('google'), (req, res) => {
+  res.redirect('/surveys');
+});
 
 module.exports = router;
